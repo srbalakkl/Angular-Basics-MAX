11df7abe04c4b66c6c7c8d85daa8947b80a580b4
srbalakkl <sr.bala.kkl@gmail.com>
2021-08-14 16:36:52 +0530

    event listener binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule
@@ -13,4 +15,6 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() {
+    setTimeout(() => {
+      this.isButtonDisable = true;
+    }, 99000);
+    this.isButtonDisable = false;
+  }
+
+  ngOnInit(): void {
+  }
+
+
+  txt :String = 'string interoperability binding ';
+  inbinding : String ='achieving interoperability binding by using property binding';
+
+  insidefn : String = "button is not clicked";
+
+  isButtonDisable = false;//property bindings
+
+
+
+  onload() {
+    // const xmlhttp = new XMLHttpRequest();
+    // const myobj = JSON.parse(this.responseText);
+    // console.log(myobj.model);
+    // xmlhttp.open("GET","./SrvFiles/connect.php",true);
+    // xmlhttp.send();
+
+    this.insidefn="button is clicked";
+  }
+}
